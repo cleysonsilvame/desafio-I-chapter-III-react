2bead42eaaaebfc5b61ae516df73736db0d28c7e
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-06-01 03:18:44 -0300

    feat: install slice machine from prismic

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,18 +1,21 @@
 import * as prismic from '@prismicio/client';
 import { HttpRequestLike } from '@prismicio/client';
 import { enableAutoPreviews } from '@prismicio/next';
+import sm from '../../sm.json';
+
+export const endpoint = sm.apiEndpoint;
 
 export interface PrismicConfig {
   req?: HttpRequestLike;
 }
 
-export function getPrismicClient(config: PrismicConfig): prismic.Client {
-  const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
+export function getPrismicClient(config: PrismicConfig = {}): prismic.Client {
+  const client = prismic.createClient(endpoint);
 
   enableAutoPreviews({
     client,
     req: config.req,
-  })
+  });
 
   return client;
 }
