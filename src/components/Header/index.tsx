fe1a281bc14564e1861a4442e379f20352a91fc8
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-06-01 03:26:15 -0300

    feat: add Header commponent

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={commonStyles.container}>
+      <header className={styles.container}>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </header>
+    </div>
+  );
 }
